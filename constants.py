4fe32c3ceb1451d9893127082c49e7c19170c3e1
xhardline <xhardline@gmail.com>
2025-10-26 00:18:08 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,21 @@
+DURATION_DAYS = 50
+
+PACKS_DAILY_DROP = {
+    'Common': 4,
+    'Uncommon': 2,
+    'Rare': 1,
+}
+
+PACKS_STATS = {
+    'Common': {'cards_per_pack': 2, 'odds': {'Common': 0.70, 'Uncommon': 0.25, 'Rare': 0.05}},
+    'Uncommon': {'cards_per_pack': 3, 'odds': {'Common': 0.55, 'Uncommon': 0.35, 'Rare': 0.10}},
+    'Rare': {'cards_per_pack': 4, 'odds': {'Common': 0.30, 'Uncommon': 0.40, 'Rare': 0.30}},
+}
+
+TOTAL_CARDS = {
+    'Common': 100,
+    'Uncommon': 60,
+    'Rare': 25,
+}
+
+DESTINATION_PATH = './monop_go_final.csv'
